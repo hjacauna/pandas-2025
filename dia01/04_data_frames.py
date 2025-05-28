8ef140d0a7e7e93c30f07a3c0c561111ec599a73
Heraldo <hjacauna@gmail.com>
2025-05-28 11:48:30 -0300

    feat: funçoes data frame

--- dia01/04_data_frames.py
+++ dia01/04_data_frames.py
@@ -4,4 +4,26 @@ import pandas as pd
 
 idades = [18, 20, 22, 19, 21, 23, 25, 24, 22, 20]
 
-indexs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+series_idades = pd.Series(idades)
+series_letras = pd.Series(letras)
+
+# %%
+
+df = pd.DataFrame()
+df["idades"] = series_idades
+df["letras"] = series_letras
+df 
+
+# %%
+
+df["letras"]
+
+# %%
+
+df.iloc[0]  # primeira linha
+
+# %%
+
+df.iloc[0]["letras"]  # primeira linha, coluna letras
